82f5d09992bf9198d8c1ee55f91c8394e6742808
Nilbe <90683548+Nilbe@users.noreply.github.com>
2022-11-28 13:33:16 +0100

    commit 2

--- Vinterprojekt2/Characters.cs
+++ Vinterprojekt2/Characters.cs
@@ -1,8 +1,26 @@
 using System;
 
-
-
 public class Characters
 {
-    
+    protected Rectangle rect;
+
+    protected Color color;
+
+
+    public Characters()
+     {
+        rect = new Rectangle(0,0,32,32);
+     }
+
+     public void Draw()
+     {
+        Raylib.DrawRectangleRec(rect, color);
+     }
+
+     public virtual void Update() 
+     {
+        
+     }
+
+    public int HP = 100;
 }
--- Vinterprojekt2/Program.cs
+++ Vinterprojekt2/Program.cs
@@ -1,3 +1,22 @@
 ﻿global using Raylib_cs;
+using RestSharp;
+using System.Text.Json;
+
+RestClient starwarsClient = new RestClient("https://swapi.py4e.com/api/");
+RestRequest request = new RestRequest("people/1/");
+
+RestResponse response = starwarsClient.GetAsync(request).Result;
+Player p = JsonSerializer.Deserialize<Player>(response.Content);
+
 
 Raylib.SetTargetFPS(60);
+Raylib.InitWindow(800,600, "GAME");
+
+while (!Raylib.WindowShouldClose())
+{
+    Raylib.BeginDrawing();
+        
+    
+
+    Raylib.EndDrawing();
+}
